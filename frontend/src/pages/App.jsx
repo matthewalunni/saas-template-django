d38926cebdf48808dca1a769308ebb22315c6a25
matthewalunni <matthew.alunni@gmail.com>
2021-10-22 19:36:45 -0400

    Update.

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,13 +1,19 @@
 // saas-template/frontend/src/components/App.js
 
-import React, { Component} from "react";
+import React, { Component } from "react";
+import NavigationBar from './components/NavigationBar';
+import Footer from './components/Footer';
 
-class App extends Component{
-  render(){
-    return(
-      <div className="site">
-        <h1>Ahhsdfjsdkfskdfjhsdfk I'm free. Time to conquer the Earth!</h1>
-      </div>
+class App extends Component {
+  render() {
+    return (
+      <>
+        <NavigationBar />
+        <div className="site">
+          <h1>Ahhsdfjsdkfskdfjhsdfk I'm free. Time to conquer the Earth!</h1>
+        </div>
+        <Footer />
+      </>
     );
   }
 }
