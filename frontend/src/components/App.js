a80e45db38157ad0ea4fdcba421c3f33ca543718
matthewalunni <matthew.alunni@gmail.com>
2021-10-21 22:41:32 -0400

    Created react app.

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+// saas-template/frontend/src/components/App.js
+
+import React, { Component} from "react";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="site">
+        <h1>Ahhh after 10,000 years I'm free. Time to conquer the Earth!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
